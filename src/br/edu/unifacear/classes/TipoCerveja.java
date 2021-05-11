bc359e1d90f7e18035a9dfca5dbbfec5fbc5aa4d
leonardo costa <leo4_almeida@hotmail.com>
2021-05-11 07:47:49 -0300

    reestrutaramento modelo de dados cerveja

---
+++ src/br/edu/unifacear/classes/TipoCerveja.java
@@ -0,0 +1,60 @@
+package br.edu.unifacear.classes;
+
+import javax.persistence.*;
+
+@Entity
+public class TipoCerveja implements IDBModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+	private String nome;
+
+
+	public TipoCerveja(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+	public TipoCerveja() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	@Override
+	public String toString() {
+		return "TipoCerveja [id=" + id + ", nome=" + nome + "]";
+	}
+
+
+	@Override
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
