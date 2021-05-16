0ad4e465ce3de1687b0ae4c773f86d81458b3d23
leonardo costa <leo4_almeida@hotmail.com>
2021-05-16 19:52:15 -0300

    IDBmodel telas

--- src/br/edu/unifacear/main/Main.java
+++ src/br/edu/unifacear/main/Main.java
@@ -6,16 +6,19 @@ package br.edu.unifacear.main;
 import br.edu.unifacear.bd.CBanco;
 import br.edu.unifacear.telas.*;
 import br.edu.unifacear.testes.ApreciadorTestes;
+import br.edu.unifacear.testes.CervejeiroTestes;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 	
 		ApreciadorTestes.saveUsuario();
+		CervejeiroTestes.saveCervejeiro();
 		CBanco.getConnection();
 		TelaLogin.telaInicial();
 		//TelaCadastroCerveja.telaCadastro();
 		TelaAdm.telaAdm();
+		
 
 	}
 
